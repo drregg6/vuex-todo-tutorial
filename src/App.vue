a058ff405b2aba652dbd1239a70ff4ec5a558cb1
Dave Regg <drregg6@gmail.com>
2019-10-13 18:14:49 -0400

    AddTodo populated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo />
       <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue';
+import Todos from './components/Todos.vue';
 
 export default {
   name: 'app',
   components: {
+    AddTodo,
     Todos
   }
 }
