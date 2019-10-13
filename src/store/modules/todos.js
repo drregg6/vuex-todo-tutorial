ca4a98dd27ad088232643545e320810bd56b8783
Dave Regg <drregg6@gmail.com>
2019-10-13 18:11:18 -0400

    AddTodo added, actions and mutations added

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const state = {
-  todos: [
-    
-  ]
+  todos: []
 };
 
 const getters = {
@@ -16,11 +14,13 @@ const actions = {
   async fetchTodos({ commit }) {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-    console.log(response);
+    commit('setTodos', response.data);
   }
 };
 
-const mutations = {};
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos)
+};
 
 export default {
   state,
