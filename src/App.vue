5febc88cfbc069b25e56bfed82a39a868e976bd4
Dave Regg <drregg6@gmail.com>
2019-10-13 19:33:55 -0400

    Added FilterTodos component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <div class="container">
       <AddTodo />
+      <FilterTodos />
       <Todos />
     </div>
   </div>
 </template>
 
 <script>
+import FilterTodos from './components/FilterTodos.vue';
 import AddTodo from './components/AddTodo.vue';
 import Todos from './components/Todos.vue';
 
 export default {
   name: 'app',
   components: {
+    FilterTodos,
     AddTodo,
     Todos
   }
